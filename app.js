38f685cb9622c48ad61d46206c7b708192fd0a81
İbrahim Ergün <ibrahimergun19@gmail.com>
2022-05-20 13:09:22 +0200

    Revert "Admin Dashboard Deploy"
    This reverts commit b916e4265db328de0952f97393fb7ce9992c54c3.

--- app.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-var app = angular.
-module("App",['ui.router','toastr','ngCookies']);
--- assets/scripts/config/app.js
+++
@@ -1,2 +0,0 @@
-'use strict';
-var app = angular.module("Arif",['ui.router','toastr','ngCookies','angularMoment','ngFileUpload']);
